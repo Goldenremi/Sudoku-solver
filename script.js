2f4b0685262dbf6b66639fae990bad3e7ee73476
Boluwatife <oderemiboluwatife@gmail.com>
2022-02-18 09:18:46 +0100

    files developed locally
    These files were worked on on a local device before being managed on github

---
+++ script.js
@@ -0,0 +1,623 @@
+//Javascript Sudoku Solver
+
+
+let board = [
+	[, , , 2, , 3, , ,],
+	[, , , , , 9, , ,],
+	[, , , 6, , 5, , ,],
+	[, , , , , , , ,],
+	[, , , , , , , ,],
+	[, , , , , , , ,],
+	[, , , 5, , 2, , ,],
+	[, , 1, , , , , ,],
+	[, , , 4, , 8, , ,],
+];
+
+board = [
+	[, , 9, , , , , ,],
+	[, 6, , , 1, , , 4],
+	[, , , 5, , 8, , ,],
+	[, , , , , 1, 7, , 2],
+	[, 1, , , , , 8, ,],
+	[, , 4, 2, 9, , , , 3],
+	[4, , , , , , , 3, 9],
+	[9, , , , 4, 3, 1, 8, 7],
+	[, , , , , , , ,],
+];
+
+board = [
+	[, , 9, , , 4, , ,],
+	[, 6, , 9, 1, , , 4],
+	[, 4, , 5, , 8, 9, ,],
+	[, 9, , 4, 8, 1, 7, , 2],
+	[, 1, , , , , 8, 9, 4],
+	[, , 4, 2, 9, , , 1, 3],
+	[4, , , , , , , 3, 9],
+	[9, , , 6, 4, 3, 1, 8, 7],
+	[, , , , , 9, 4, ,],
+];
+
+for (let i = 0; i < 9; i++) {
+	for (let j = 0; j < 9; j++) {
+		if (board[i][j] == null || board[i][j] == undefined) {
+			board[i][j] = 0;
+		}
+	}
+}
+
+const createBoard = () => {
+	let CBboard = [];
+	let row = [];
+	for (let i = 0; i < 9; i++) {
+		row.push("");
+	}
+	for (let i = 0; i < 9; i++) {
+		CBboard.push(row);
+	}
+	return CBboard;
+};
+
+const receiveInput = (RIboard) => {
+	for (let i = 0; i < 9; i++) {
+		let notCorrectLenght = true;
+		do {
+			data = prompt(
+				"Input data for row " + (i + 1) + " seperated by comma's"
+			);
+			data = data.split(",");
+			if (data.length === 9) {
+				notCorrectLenght = false;
+			}
+		} while (notCorrectLenght);
+	}
+
+	RIboard[i] = data;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			if (RIboard[i][j] === "") {
+				RIboard[i][j] = 0;
+			}
+		}
+	}
+	return RIboard;
+};
+
+const boardate = (BboardObject) => {
+	board = [];
+	for (i = 0; i < 9; i++) {
+		row = [];
+		board.push(row);
+	}
+
+	for (j = 0; j < 81; j++) {
+		board[BboardObject.cells[j].row].push(BboardObject.cells[j].value);
+	}
+	return board;
+};
+
+const displayBoard = (DBboard) => {
+	for (let i = 0; i < 9; i++) {
+		string = " ";
+		for (let j = 0; j < 9; j++) {
+			if (DBboard[i][j] === 0) {
+				string = string + "- ";
+			} else {
+				string = string + DBboard[i][j].toString() + " ";
+			}
+		}
+		console.log(string);
+	}
+};
+
+const makeGameObject = (MGOboard) => {
+	let gameObject = {
+		cells: [],
+		rows: [],
+		columns: [],
+		boxes: [],
+	};
+
+	//Game object cells
+	let cells = [];
+	let cell = {};
+	for (let i = 0; i < 9; i++) {
+		let row = [];
+		for (let j = 0; j < 9; j++) {
+			cell = {
+				id: "",
+				usefulId: "",
+				row: "",
+				column: "",
+				box: "",
+				value: "",
+				possibleValues: [],
+			};
+			cell["value"] = MGOboard[i][j];
+			cell["id"] = (i + 1).toString() + (j + 1).toString();
+			let converter = Number(i.toString() + j.toString());
+			let subtracter = Math.floor(converter / 10);
+			cell["usefulId"] = converter - subtracter;
+			cell["row"] = i;
+			cell["column"] = j;
+
+			//cell possibleValues
+			if (cell.value === 0) {
+				cell["possibleValues"] = [...[1, 2, 3, 4, 5, 6, 7, 8, 9]];
+			} else {
+				cell["possibleValues"] = [cell.value];
+			}
+
+			//cell box
+			if (cell.row < 3) {
+				if (cell.column < 3) {
+					cell["box"] = 0;
+				} else if (3 <= cell.column && cell.column <= 5) {
+					cell["box"] = 3;
+				} else if (cell.column > 5) {
+					cell["box"] = 6;
+				}
+			} else if (3 <= cell.row && cell.row <= 5) {
+				if (cell.column < 3) {
+					cell["box"] = 1;
+				} else if (3 <= cell.column && cell.column <= 5) {
+					cell["box"] = 4;
+				} else if (cell.column > 5) {
+					cell["box"] = 7;
+				}
+			} else if (cell.row > 5) {
+				if (cell.column < 3) {
+					cell["box"] = 2;
+				} else if (3 <= cell.column && cell.column <= 5) {
+					cell["box"] = 5;
+				} else if (cell.column > 5) {
+					cell["box"] = 8;
+				}
+			}
+			gameObject["cells"].push({ ...cell });
+		}
+	}
+
+	// Game object rows and columns
+
+	let rows = [];
+	let columns = [];
+	let boxes = [];
+	for (let i = 0; i < 9; i++) {
+		rows.push({
+			...{
+				id: (i + 1).toString(),
+				name: "row",
+				boxes: [],
+				cellsId: [],
+				knownCellsId: [],
+				unknownCellsId: [],
+				values: [],
+				valuesLeft: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+			},
+		});
+
+		columns.push({
+			...{
+				id: (i + 1).toString(),
+				name: "column",
+				boxes: [],
+				cellsId: [],
+				knownCellsId: [],
+				unknownCellsId: [],
+				values: [],
+				valuesLeft: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+			},
+		});
+
+		boxes.push({
+			...{
+				id: (i + 1).toString(),
+				name: "box",
+				cellsId: [],
+				knownCellsId: [],
+				unknownCellsId: [],
+				rows: [],
+				columns: [],
+				values: [],
+				valuesLeft: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+			},
+		});
+	}
+	gameObject.rows.push(...rows);
+	gameObject.columns.push(...columns);
+	gameObject.boxes.push(...boxes);
+
+	for (let i = 0; i < 81; i++) {
+		let id = gameObject.cells[i].usefulId;
+		let row = gameObject.cells[i].row;
+		let column = gameObject.cells[i].column;
+		let box = gameObject.cells[i].box;
+		let value = gameObject.cells[i].value;
+
+		gameObject.rows[row].cellsId.push(id);
+		if (value === 0) {
+			gameObject.rows[row].unknownCellsId.push(id);
+		} else {
+			gameObject.rows[row].knownCellsId.push(id);
+			gameObject.rows[row].values.push(value);
+			gameObject.rows[row].valuesLeft.splice(
+				gameObject.rows[row].valuesLeft.indexOf(value),
+				1
+			);
+		}
+		if (gameObject.rows[row].boxes.indexOf(box) == -1) {
+			gameObject.rows[row].boxes.push(box);
+		}
+
+		gameObject.columns[column].cellsId.push(id);
+		if (value === 0) {
+			gameObject.columns[column].unknownCellsId.push(id);
+		} else {
+			gameObject.columns[column].knownCellsId.push(id);
+			gameObject.columns[column].values.push(value);
+			gameObject.columns[column].valuesLeft.splice(
+				gameObject.columns[column].valuesLeft.indexOf(value),
+				1
+			);
+		}
+		if (gameObject.columns[column].boxes.indexOf(box) == -1) {
+			gameObject.columns[column].boxes.push(box);
+		}
+
+		gameObject.boxes[box].cellsId.push(id);
+		if (value === 0) {
+			gameObject.boxes[box].unknownCellsId.push(id);
+		} else {
+			gameObject.boxes[box].knownCellsId.push(id);
+			gameObject.boxes[box].values.push(value);
+			gameObject.boxes[box].valuesLeft.splice(
+				gameObject.boxes[box].valuesLeft.indexOf(value),
+				1
+			);
+		}
+
+		if (gameObject.boxes[box].rows.indexOf(row) == -1) {
+			gameObject.boxes[box].rows.push(row);
+		}
+		if (gameObject.boxes[box].columns.indexOf(column) == -1) {
+			gameObject.boxes[box].columns.push(column);
+		}
+	}
+	return gameObject;
+};
+
+
+const strategyScanning = (SSGameObject) => {
+	SSGameObject = JSON.parse(JSON.stringify(SSGameObject));
+	for (let i = 0; i < 81; i++) {
+		let row = SSGameObject.cells[i].row;
+		let column = SSGameObject.cells[i].column;
+		let box = SSGameObject.cells[i].box;
+		let value = SSGameObject.cells[i].value;
+
+		if (value === 0) {
+			SSGameObject.cells[i].possibleValues = [
+				...SSGameObject.cells[i].possibleValues.filter((x) =>
+					SSGameObject.rows[row].valuesLeft.includes(x)
+				),
+			];
+			SSGameObject.cells[i].possibleValues = [
+				...SSGameObject.cells[i].possibleValues.filter((x) =>
+					SSGameObject.columns[column].valuesLeft.includes(x)
+				),
+			];
+			SSGameObject.cells[i].possibleValues = [
+				...SSGameObject.cells[i].possibleValues.filter((x) =>
+					SSGameObject.boxes[box].valuesLeft.includes(x)
+				),
+			];
+			if (SSGameObject.cells[i].possibleValues.length === 1) {
+				value = SSGameObject.cells[i].possibleValues[0];
+				SSGameObject = JSON.parse(
+					JSON.stringify(
+						updater(0, SSGameObject, i, value, undefined)
+					)
+				);
+			}
+		}
+	}
+	return SSGameObject;
+};
+
+const stratergyExclusiveCells = (SECGameObject) => {
+	let cellsId;
+	let possibleValues;
+	let value;
+	let inIt;
+
+	for (let i = 1; i < 10; i++) {
+		for (let j = 0; j < 9; j++) {
+			cellsId = [...SECGameObject.boxes[j].cellsId];
+			inIt = [];
+			for (let k = 0; k < 9; k++) {
+				possibleValues = [
+					...SECGameObject.cells[cellsId[k]].possibleValues,
+				];
+				value = SECGameObject.cells[cellsId[k]].value;
+				if (possibleValues.includes(i) && value === 0) {
+					inIt.push(
+						JSON.parse(
+							JSON.stringify(SECGameObject.cells[cellsId[k]])
+						)
+					);
+				}
+			}
+
+			let a;
+			let SECArray;
+			if (inIt.length !== 0) {
+				a = inIt.reduce(
+					(acc, x) => inIt[0].column === x.column && acc,
+					true
+				);
+			}
+			if (a === true) {
+				//console.log(inIt,i)
+				SECArray = SECGameObject.columns[
+					inIt[0].column
+				].unknownCellsId.filter(
+					(x) =>
+						SECGameObject.cells[x].possibleValues.includes(i) &&
+						SECGameObject.cells[x].box !== j
+				);
+				//console.log(SECGameObject.columns[inIt[0].column].unknownCellsId)
+				SECGameObject = JSON.parse(
+					JSON.stringify(
+						updater(1, SECGameObject, undefined, [i], [...SECArray])
+					)
+				);
+			}
+		}
+	}
+	return SECGameObject;
+};
+
+const advancedScanning = (ASgameObject) => {
+	//Slow, Notcompleted
+	/*for (let i = 0; i < 9; i++) {
+		ASgameObject.boxes[i].cellsId.forEach((value_one) => {
+			ASgameObject.cells[value_one].possibleValues.((value_two)=>{
+				for (let j = 0; j < 9; j++) {
+					ASgameObject.boxes[i].cellsId.forEach((value) => {
+			ASgameObject.cells[value].possibleValues.((value)=>{
+			})
+			})
+		})*/
+
+	ASgameObject = JSON.parse(JSON.stringify(ASgameObject));
+	let cellsId;
+	let possibleValues;
+	let value;
+	let inIt;
+
+	const repeat = (x) => {
+		for (let i = 1; i < 10; i++) {
+			for (let j = 0; j < 9; j++) {
+				cellsId = [...x[j].cellsId];
+				inIt = [];
+				for (let k = 0; k < 9; k++) {
+					possibleValues = [
+						...ASgameObject.cells[cellsId[k]].possibleValues,
+					];
+					value = ASgameObject.cells[cellsId[k]].value;
+					if (possibleValues.includes(i) && value === 0) {
+						inIt.push(
+							JSON.parse(
+								JSON.stringify(ASgameObject.cells[cellsId[k]])
+							)
+						);
+					}
+				}
+
+				if (inIt.length === 1) {
+					let id = ASgameObject.cells[inIt[0].usefulId].usefulId;
+					let value = i;
+					ASgameObject = JSON.parse(
+						JSON.stringify(
+							updater(0, ASgameObject, id, value, undefined)
+						)
+					);
+				} else if (inIt.length > 1) {
+					const repeatTwo = (a, b) => {
+						let ASgameArray = [];
+						let ASIgnore = [];
+						for (let l = 0; l < b.length; l++) {
+							ASgameArray.push(b[l].usefulId);
+						}
+						for (let m = 0; m < a.length; m++) {
+							ASIgnore.push(a[m].usefulId);
+						}
+						if (ASgameArray.length !== ASIgnore.length) {
+							ASgameArray = ASgameArray.filter(
+								(x) => !ASIgnore.includes(x)
+							);
+							ASgameObject = JSON.parse(
+								JSON.stringify(
+									updater(
+										1,
+										ASgameObject,
+										undefined,
+										[
+											...ASgameObject.cells[ASIgnore[0]]
+												.possibleValues,
+										],
+										ASgameArray
+									)
+								)
+							);
+							if (a.length === 2) {
+								console.log("2");
+							}
+							if (a.length === 3) {
+								console.log("3");
+							}
+						}
+					};
+					let caseInit = JSON.parse(JSON.stringify(inIt));
+					caseTwoInit = caseInit.filter(
+						(x) => x.possibleValues.length === 2
+					);
+					caseThreeInit = caseInit.filter(
+						(x) => x.possibleValues.length === 3
+					);
+					caseFourInit = caseInit.filter(
+						(x) => x.possibleValues.length === 4
+					);
+
+					if (
+						caseTwoInit.length === 2 &&
+						JSON.stringify(caseTwoInit[0].possibleValues) ===
+							JSON.stringify(caseTwoInit[1].possibleValues) &&
+						caseTwoInit[0].possibleValues.length === 2
+					) {
+						repeatTwo(caseTwoInit, inIt);
+					}
+
+					if (
+						caseThreeInit.length === 3 &&
+						(JSON.stringify(caseThreeInit[0].possibleValues) ===
+							JSON.stringify(caseThreeInit[1].possibleValues)) ===
+							JSON.stringify(caseThreeInit[2].possibleValues) &&
+						caseThreeInit[0].possibleValues.length === 3
+					) {
+						console.log(3);
+						repeatTwo(caseThreeInit, inIt);
+					}
+				}
+			}
+		}
+		return ASgameObject;
+	};
+	return repeat(repeat(repeat(ASgameObject.boxes).rows).columns);
+};
+
+const updater = (UMode, UObject, UId, UValue, UArray) => {
+	UObject = JSON.parse(JSON.stringify(UObject));
+	if (UMode === 0) {
+		let Urow = UObject.cells[UId].row;
+		let Ucolumn = UObject.cells[UId].column;
+		let Ubox = UObject.cells[UId].box;
+		let UpossibleValues = [...UObject.cells[UId].possibleValues];
+
+		if (
+			UpossibleValues.indexOf(UValue) !== -1 &&
+			UObject.rows[Urow].valuesLeft.indexOf(UValue) !== -1 &&
+			UObject.columns[Ucolumn].valuesLeft.indexOf(UValue) !== -1 &&
+			UObject.boxes[Ubox].valuesLeft.indexOf(UValue) !== -1
+		) {
+			UObject.cells[UId].value = UValue;
+			UObject.cells[UId].possibleValues = [UValue];
+
+			UObject.columns[Ucolumn].unknownCellsId = [
+				...UObject.columns[Ucolumn].unknownCellsId.filter(
+					(x) => x !== UId
+				),
+			];
+			UObject.columns[Ucolumn].knownCellsId.push(UId);
+			UObject.columns[Ucolumn].values.push(UValue);
+			UObject.columns[Ucolumn].valuesLeft.splice(
+				UObject.columns[Ucolumn].valuesLeft.indexOf(UValue),
+				1
+			);
+
+			UObject.rows[Urow].unknownCellsId = [
+				...UObject.rows[Urow].unknownCellsId.filter((x) => x !== UId),
+			];
+			UObject.rows[Urow].knownCellsId.push(UId);
+			UObject.rows[Urow].values.push(UValue);
+			UObject.rows[Urow].valuesLeft.splice(
+				UObject.rows[Urow].valuesLeft.indexOf(UValue),
+				1
+			);
+
+			UObject.boxes[Ubox].unknownCellsId = [
+				...UObject.boxes[Ubox].unknownCellsId.filter((x) => x !== UId),
+			];
+			UObject.boxes[Ubox].knownCellsId.push(UId);
+			UObject.boxes[Ubox].values.push(UValue);
+			UObject.boxes[Ubox].valuesLeft.splice(
+				UObject.boxes[Ubox].valuesLeft.indexOf(UValue),
+				1
+			);
+
+			for (let i = 0; i < 9; i++) {
+				let boxCells = [...UObject.boxes[Ubox].cellsId];
+				y = UObject.cells[boxCells[i]].value;
+				let z =
+					UObject.cells[boxCells[i]].possibleValues.indexOf(UValue);
+				if (z !== -1 && y !== UValue) {
+					UObject.cells[boxCells[i]].possibleValues.splice(z, 1);
+				}
+			}
+
+			for (let i = 0; i < 9; i++) {
+				let rowCells = [...UObject.rows[Urow].cellsId];
+				y = UObject.cells[rowCells[i]].value;
+				let z =
+					UObject.cells[rowCells[i]].possibleValues.indexOf(UValue);
+				if (z !== -1 && y !== UValue) {
+					UObject.cells[rowCells[i]].possibleValues.splice(z, 1);
+				}
+			}
+
+			for (let i = 0; i < 9; i++) {
+				let columnCells = [...UObject.columns[Ucolumn].cellsId];
+				y = UObject.cells[columnCells[i]].value;
+				let z =
+					UObject.cells[columnCells[i]].possibleValues.indexOf(
+						UValue
+					);
+				if (z !== -1 && y !== UValue) {
+					UObject.cells[columnCells[i]].possibleValues.splice(z, 1);
+				}
+			}
+		} else {
+			console.log("Invalid Update", UId, UValue);
+		}
+	} else if (UMode === 1) {
+		for (let i = 0; i < UArray.length; i++) {
+			//console.log(UObject.cells[UArray[i]].possibleValues,UValue)
+			UObject.cells[UArray[i]].possibleValues = [
+				...UObject.cells[UArray[i]].possibleValues.filter(
+					(x) => !UValue.includes(x)
+				),
+			];
+			//console.log(UObject.cells[UArray[i]].possibleValues,UValue)
+		}
+	}
+
+	return UObject;
+};
+
+const solver = (SObject, Stratergies) => {
+	let doneBoard_old;
+	let doneBoard_new = {};
+	let i = 0;
+	do {
+		console.log("solving ", i);
+		i++;
+		doneBoard_old = JSON.parse(JSON.stringify(doneBoard_new));
+		doneBoard_new = SObject = Stratergies.reduce(
+			(acc, x) => JSON.parse(JSON.stringify(x(acc))),
+			JSON.parse(JSON.stringify(SObject))
+		);
+	} while (
+		JSON.stringify(doneBoard_old) !== JSON.stringify(doneBoard_new) &&
+		i < 100
+	);
+
+	return doneBoard_new;
+};
+let gameObject = makeGameObject(board);
+let solvedGameObject = solver(gameObject, [
+	strategyScanning,
+	stratergyExclusiveCells,
+	advancedScanning,
+]);
+displayBoard(boardate(solvedGameObject));
+console.log(solvedGameObject);
---
+++ scriptOne.js
@@ -0,0 +1,167 @@
+/*The sudoku board was represented by an array with respect to of rows only,
+which was useful for one stratergy only. 
+In future, i'll learn all about the problems to be solved before solving them. 
+
+
+Most function varibles were prefixed with an abbreviation of the board,
+even though their scope is within the function only.
+
+Also,part of the code below might be modified, 
+as they were not moved to this seperate file until much later,
+and parts were copied and pasted from main file 
+*/
+
+
+//Creation of the empty board, a 9 by 9 array of empty strings
+const createBoard = () => {
+	let CBboard = [];
+	let row = [];
+	for (let i = 0; i < 9; i++) {
+		row.push("");
+	}
+	for (let j = 0; j < 9; j++) {
+		CBboard.push(row);
+	}
+	return CBboard;
+};
+
+// Receiving input via prompt during development, later updated
+const receiveInput = (RIboard) => {
+	for (let i = 0; i < 9; i++) {
+		let notCorrectLenght = true;
+		do {
+			data = prompt(
+				"Input data for row " + (i + 1) + " seperated by comma's"
+			);
+			data = data.split(",");
+			if (data.length === 9) {
+				notCorrectLenght = false;
+			}
+		} while (notCorrectLenght);
+	}
+
+	RIboard[i] = data;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			if (RIboard[i][j] === "") {
+				RIboard[i][j] = 0;
+			}
+		}
+	}
+	return RIboard;
+};
+
+
+//Used in main script file to sort needed values in order
+const boardate = (BboardObject) => {
+	board = [];
+	for (i = 0; i < 9; i++) {
+		row = [];
+		board.push(row);
+	}
+
+	for (j = 0; j < 81; j++) {
+		board[BboardObject.cells[j].row].push(BboardObject.cells[j].value);
+	}
+	return board;
+};
+
+//Logging in a suitable view to the console,used in main script file during development
+const displayBoard = (DBboard) => {
+	for (let i = 0; i < 9; i++) {
+		string = " ";
+		for (let j = 0; j < 9; j++) {
+			if (DBboard[i][j] === 0) {
+				string = string + "- ";
+			} else {
+				string = string + DBboard[i][j].toString() + " ";
+			}
+		}
+		console.log(string);
+	}
+};
+
+//Derivation of rows and columns, as 9 by 9 array
+function representation (Rboard){
+	let rows = Rboard
+	let columns = []
+	for (let i = 0; i < 9 ; i++){
+		columns.push([])
+	}
+
+	for (let i = 0; i < 9 ; i++){
+		for (let j = 0; j < 9 ; j++){
+			columns[i].push(Rboard[j][i])
+		}
+	}
+	return([rows,columns])
+}
+
+//Derivation of boxes, also as 9 by 9 array
+function makeBoxes(MBboard){
+	let boxes=[]
+	for (i=0; i<9; i++){
+		boxes.push([])
+	}
+	
+	for (let i=0; i<9; i=i+3){
+		for (let j=0; j<3; j++){
+			boxes[i].push(MBboard[i][j])
+			boxes[i].push(MBboard[i+1][j])
+			boxes[i].push(MBboard[i+2][j])
+			boxes[i+1].push(MBboard[i][j+3])
+			boxes[i+1].push(MBboard[i+1][j+3])
+			boxes[i+1].push(MBboard[i+2][j+3])
+			boxes[i+2].push(MBboard[i][j+6])
+			boxes[i+2].push(MBboard[i+1][j+6])
+			boxes[i+2].push(MBboard[i+2][j+6])
+		}
+	}
+
+	return(boxes) }
+
+//Initial solution function
+function solution(common){
+	for(i=0; i<9 ;i++){
+		for (j = 0 ; j<9 ; j++){
+			if (common[i][j]['value'] !==0){
+			 	 for (k=0; k<9; k++){
+			 	 	let index = common[i][k]['possibleValues'].indexOf(common[i][j]['value'])
+			 	 	if (common[i][k] !== common[i][j] && index !==-1 ){	
+						common[i][k]['possibleValues'].splice(index,1)	
+			 	}
+				else if(common[i][k] === common[i][j]){common[i][k]['possibleValues'] = [common[i][j]['value']]
+				}
+				}
+			}
+
+			if (common[i][j]['possibleValues'].length === 1){
+				common[i][j]['value'] = common[i][j]['possibleValues'][0]}
+		}
+	}
+	return (common)
+}
+
+function runner(solution,Rrows,Rcolumns,Rboxes,RboardArray){
+	let newBoardArray 
+	let oldboardArray
+	do{
+		newBoardArray = RboardArray
+		oldboardArray = JSON.parse(JSON.stringify(newBoardArray))
+		solution(Rrows)
+		solution(Rcolumns)
+		solution(Rboxes)
+		newBoardArray = RboardArray
+		i++
+	}
+	while(JSON.stringify(newBoardArray) !== JSON.stringify(oldboardArray))
+	return RboardArray
+}
+
+boardArray = dictionate(board)
+let [rows,columns] = representation(boardArray)
+let boxes = makeBoxes(boardArray)
+displayBoard(boardate(boardArray))
+boardArray = runner(solutionScanning, rows, columns, boxes, boardArray)
+displayBoard(boardate(boardArray))
+
